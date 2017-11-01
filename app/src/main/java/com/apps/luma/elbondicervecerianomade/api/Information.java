5509ab105ddfdcc924ea2a592bc02a8c7fe0098b
Jose Antonini <jocantonini@gmail.com>
2017-11-01 18:52:11 -0300

    Pedido api

---
+++ app/src/main/java/com/apps/luma/elbondicervecerianomade/api/Information.java
@@ -0,0 +1,44 @@
+package com.apps.luma.elbondicervecerianomade.api;
+
+import android.util.Log;
+
+import com.apps.luma.elbondicervecerianomade.MenuActivity;
+import com.apps.luma.elbondicervecerianomade.R;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by Usuario on 01/11/2017.
+ */
+
+public class Information {
+    private MenuActivity menuActivity;
+    private List<JSONObject> datos = new ArrayList<>();
+
+    public Information(MenuActivity menuActivity){
+        this.menuActivity = menuActivity;
+        hacerPedidos();
+    }
+
+    private void hacerPedidos(){
+        Pedido p1 = new Pedido(menuActivity.getResources().getString(R.string.locations_url), this);
+        p1.execute();
+        Pedido p2 = new Pedido(menuActivity.getResources().getString(R.string.products_url), this);
+        p2.execute();
+
+    }
+
+    public void resivirInfo(JSONObject s) throws JSONException {
+        datos.add(s);
+        Log.d("JSONNN", s.toString(3));
+        if(datos.size()==2){
+            menuActivity.receptionLocations(datos.get(0),datos.get(1));
+        }
+    }
+}
+
+
---
+++ app/src/main/java/com/apps/luma/elbondicervecerianomade/api/Pedido.java
@@ -0,0 +1,72 @@
+package com.apps.luma.elbondicervecerianomade.api;
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+/**
+ * Created by Usuario on 01/11/2017.
+ */
+
+public class Pedido extends AsyncTask<String, Void, String> {
+
+    private String url;
+    private Information info;
+
+    public Pedido(String url, Information info) {
+        this.url = url;
+        this.info = info;
+    }
+
+    @Override
+    protected String doInBackground(String... params) {
+
+        OkHttpClient client = new OkHttpClient();
+
+        Request request = new Request.Builder()
+                .url(url)
+                .build();
+
+        Log.d("HEADERS", request.toString());
+
+        Response res = null;
+        try {
+            res = client.newCall(request).execute();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        String result = null;
+        try {
+            result = res.body().string();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+
+    @Override
+    protected void onPostExecute(String s) {
+        try {
+            JSONObject obj = new JSONObject(s);
+            info.resivirInfo(obj);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }finally {
+            try {
+                finalize();
+            } catch (Throwable throwable) {
+                throwable.printStackTrace();
+            }
+        }
+
+    }
+}
